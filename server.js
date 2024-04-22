268ecb561f4c103f6b5ba5ca4b584a03a18ca4bd
HDehji <hosseindehji007@gmail.com>
2024-04-22 12:42:15 +0330

    db added

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
-const express = require('express')
-const config = require('./config')
-const app = express()
+const express = require('express');
+const config = require('./config');
+const app = express();
+const{check,validationResult}=require('express-validator');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const flash = require('connect-flash');
+const mongoose = require('mongoose');
+require('dotenv').config()
 
-global.config=require('./config')
+mongoose.connect('mongodb://127.0.0.1:27017/test').then(() => console.log('Connected!'));
 
-app.use(express.static(__dirname + "/public"))
+global.config=require('./config');
 
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({extends:false}));
+app.use(methodOverride('method'));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(flash());
+app.use('/',require('./routes/users'));
+
+
+app.set('view engin','ejs');
 
 app.listen(config.port,()=>{
     console.log(`server is running on port ${config.port}`);
-})
+});
