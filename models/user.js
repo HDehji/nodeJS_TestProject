701c04855811af5caa62f72812d4b54d5eacdb9f
HDehji <hosseindehji007@gmail.com>
2024-04-24 00:33:44 +0330

    model added

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ const Schema=mongoose.Schema;
 const userSchema=new Schema({
     first_name:{type:String,},
     email:{type:String,required:true},
-    password:{type:String,required:true}
+    password:{type:String,required:true},
+    
 
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,38 @@
 const express = require('express');
 const router=express.Router();
+const User=require("./../models/user")
+const{check,validationResult}=require('express-validator');
 
-router.get("/",function(req,res){
-    res.send("FU")
+
+router.get("/",async function(req,res){
+    let users=await User.find({});
+    res.status(200).json({
+        data:users,
+        success:true
+    })
 });
+
+router.post("/",[
+    check('email','فرمت ایمیل صحیح نمی باشد').isEmail(),
+    check('password','طول پسورد باید حداقل 5 کاراکتر باشد').isLength({min:5})
+],async function(req,res){
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(422).json({errors:errors.array()})
+    };
+
+    let newUser=new User({
+        first_name:req.body.first_name,
+        email:req.body.email,
+        password:req.body.password,        
+    });
+    await newUser.save();
+    res.status(200).json({
+        data:"یوزر جدید با موفقعت ثبت شد",
+        success:true
+    })
+});
+
+router.put("")
 
 module.exports=router;
